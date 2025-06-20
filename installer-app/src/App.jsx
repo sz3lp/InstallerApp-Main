76e4673d846bb1bdc0927b500daf0407bbd8102f
sz3lp <luke@ospreyexterior.com>
2025-06-19 21:02:02 -0700

    fix tests and modal focus trap

--- installer-app/src/App.jsx
+++ installer-app/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import InstallerHomePage from './installer/pages/InstallerHomePage';
-import AppointmentSummaryPage from './installer/pages/AppointmentSummaryPage';
-import ActivitySummaryPage from './installer/pages/ActivitySummaryPage';
-import JobDetailPage from './installer/pages/JobDetailPage';
-import IFIDashboard from './installer/pages/IFIDashboard';
-import MockJobsPage from './installer/pages/MockJobsPage';
-import FeedbackPage from './installer/pages/FeedbackPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import InstallerHomePage from "./installer/pages/InstallerHomePage";
+import AppointmentSummaryPage from "./installer/pages/AppointmentSummaryPage";
+import ActivitySummaryPage from "./installer/pages/ActivitySummaryPage";
+import JobDetailPage from "./installer/pages/JobDetailPage";
+import IFIDashboard from "./installer/pages/IFIDashboard";
+import MockJobsPage from "./installer/pages/MockJobsPage";
+import FeedbackPage from "./installer/pages/FeedbackPage";
+import InstallManagerDashboard from "./app/install-manager/page.jsx";
 
 const App = () => (
   <Router>
@@ -17,6 +18,7 @@ const App = () => (
       <Route path="/ifi" element={<IFIDashboard />} />
       <Route path="/job/:jobId" element={<JobDetailPage />} />
       <Route path="/mock-jobs" element={<MockJobsPage />} />
+      <Route path="/install-manager" element={<InstallManagerDashboard />} />
       <Route path="/feedback" element={<FeedbackPage />} />
     </Routes>
   </Router>
