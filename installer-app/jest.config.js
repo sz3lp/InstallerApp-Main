14d02dbac326b260b01623536000a915ea93f5a3
sz3lp <luke@ospreyexterior.com>
2025-06-19 22:10:16 -0700

    chore: remove debug console log

--- installer-app/jest.config.js
+++ installer-app/jest.config.js
@@ -1,5 +1,6 @@
 export default {
   testEnvironment: 'jsdom',
+  setupFiles: ['./jest.setup.js'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleNameMapper: {
     '^react$': 'react',
